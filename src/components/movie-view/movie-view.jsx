8a123f421df12c3552b2c8bcd8d56dc094f068ab
tcerini <tommycerini@gmail.com>
2023-05-31 12:40:12 +0200

    image path updated

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,11 +2,15 @@ export const MovieView = ({ movie, onBackClick }) => {
     return (
       <div>
         <div>
-          <img src={movie.image} />
+          <img src={movie.imagePath} />
         </div>
         <div>
           <span>Title: </span>
           <span>{movie.title}</span>
+        </div>
+        <div>
+          <span>Description: </span>
+          <span>{movie.description}</span>
         </div>
         <button onClick={onBackClick}>Back</button>
       </div>
