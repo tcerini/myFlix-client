09668197543f920ce241cf62b791a36e04eb13e1
tcerini <tommycerini@gmail.com>
2023-06-09 12:47:22 +0200

    login view state if(!user)

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,23 +1,26 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
   
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     fetch("https://tc-movie-api.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-
         setMovies(data)
-
       });
   }, []);
+
+  if (!user) {
+    return <LoginView />;
+  }
 
   if (selectedMovie) {
     return (
