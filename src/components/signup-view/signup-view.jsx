09d97cf090f374f96f790d355ea2cd623953aada
tcerini <tommycerini@gmail.com>
2023-06-13 17:12:27 +0200

    signup url fix

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -16,7 +16,7 @@ export const SignupView = () => {
       Birthday: birthday
     };
 
-    fetch("https://tc-movie-api.herokuapp.com/user", {
+    fetch("https://tc-movie-api.herokuapp.com/users", {
       method: "POST",
       body: JSON.stringify(data),
       headers: {
