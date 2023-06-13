6ee19877cd8a3444b188da84da800930d03978f7
tcerini <tommycerini@gmail.com>
2023-06-13 15:52:08 +0200

    signup-view added

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   
@@ -29,12 +30,16 @@ export const MainView = () => {
 
   if (!user) {
     return (
+      <>
       <LoginView 
         onLoggedIn={(user, token) => {
           setUser(user);
           setToken(token);
         }}
       />
+      or
+      <SignupView />
+      </>
     );
   }
 
@@ -61,7 +66,7 @@ export const MainView = () => {
           />
         ))}
       </div>  
-      <button onClick={() => { setUser(null); setToken(null); }}>Logout</button>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
     </div>
   );
 
