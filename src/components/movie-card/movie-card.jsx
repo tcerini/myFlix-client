c4420a311a62d1a9409badeaf8e99a297eddece6
tcerini <tommycerini@gmail.com>
2023-06-20 13:06:40 +0200

    add <card> element to movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,14 +1,19 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 // The MovieCard function component 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}>
-      {movie.Title}
-    </div>
+    <Card>
+      <Card.Img variant="top" src={movie.imagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Director.Name}</Card.Text>
+        <Button onClick={() => onMovieClick(movie)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
