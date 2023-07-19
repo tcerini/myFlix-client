1a49ad0513648bb781ae36d292ffc75763861395
tcerini <tommycerini@gmail.com>
2023-07-19 13:48:53 +0200

    added movie-card for favourite movies

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,14 +1,20 @@
 import { useState, useEffect } from "react";
+import { MovieCard } from '../movie-card/movie-card';
 import Button from "react-bootstrap/Button";
 import { Col, Container, Card } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
-export const ProfileView = ({ user, token, setUser, movies, onLogout }) => {
+export const ProfileView = ({ user, token, setUser, movies, movie, onLogout }) => {
     const [username, setUsername] = useState(user.Username);
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState(user.Email);
-    const [birthday, setBirthday] = useState(user.Birthday); 
+    const [birthday, setBirthday] = useState(user.Birthday);
+   
+    const favouriteMovies = movies.filter((movie) => {
+        return user.FavouriteMovies.includes(movie._id)
+    });
+
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -39,6 +45,35 @@ export const ProfileView = ({ user, token, setUser, movies, onLogout }) => {
             setUser(data);
         })
     };
+
+    const removeFavourite = () => {
+        fetch(
+          `https://tc-movie-api.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
+          {
+            method: 'DELETE',
+            headers: { Authorization: `Bearer ${token}` }
+          }
+        )
+  
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            return false;
+          }
+        })
+  
+        .then((user) => {
+          if (user) {
+            setFavourite(false);
+            updateUser(user);
+          }
+        })
+        
+        .catch((e) => {
+          alert(e);
+        });
+    };  
 
     // const handleSubmit = (event) => {
     //     // this prevents the default behavior of the form which is to reload the entire page
@@ -95,11 +130,18 @@ export const ProfileView = ({ user, token, setUser, movies, onLogout }) => {
                         Email: {user.Email}
                     </Card.Text>
                     </Col>
-                    <Col className='mt-2'>
-                    <Card.Text> 
-                        Favourite Movies: {user.FavouriteMovies}
-                    </Card.Text>
-                    </Col>
+                    <Row>
+                        <Card.Text className='mt-2'> 
+                            Favourite Movies:
+                        </Card.Text>
+                            {favouriteMovies.map((movie) => (
+                                <Col className='mt-2' key={movie._id} md={4}>
+                                    <MovieCard movie={movie} className="shadow p-3 mb-5 bg-white rounded card-aspects"></MovieCard>
+                                    <Button movie={movie} onClick={removeFavourite}>Remove from favourites</Button>  
+                                </Col>
+                            ))}
+                    </Row> 
+                
                 </div>
             </Container>
         </>         
